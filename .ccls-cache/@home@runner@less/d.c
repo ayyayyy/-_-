989053e116bbba644491d921ed29cd49a5144936
ayyayyy <128569836+ayyayyy@users.noreply.github.com>
2023-05-16 06:25:06 +0000

    스위치문 끝

---
+++ .ccls-cache/@home@runner@less/d.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// main(){
+//   int a;
+//   printf("점수 입력");
+//   scanf("%d",&a);
+//   if(a>=90)
+//     printf("A학점");
+//   else if(a>=80)
+//     printf("B학점"); 
+//   else if(a>=70)
+//     printf("C학점");
+//   else if(a>=60)
+//     printf("D학점");
+//   else
+//     printf("F학점");
+// }
