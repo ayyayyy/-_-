a66ce0cc3278adb326fd238bbfc230c64d2c5b5d
ayyayyy <128569836+ayyayyy@users.noreply.github.com>
2023-05-23 06:12:28 +0000

    while문

--- .ccls-cache/@home@runner@less/f.c
+++ .ccls-cache/@home@runner@less/f.c
@@ -1,22 +1,14 @@
-#include<stdio.h>
-main(){
-  int a;
-  printf("1.사과 2.키위 3.복숭아 4.수박" );
-  scanf("%d",&a);
-  switch(a){
-  case 1:
-    printf("사과를 좋아하는군요!");
-    break;
-  case 2:
-    printf("키위를 좋아하는군요!");
-    break;
-  case 3:
-    printf("복숭아를 좋아하는군요!");
-    break;
-  case 4:
-    printf("수박을 좋아하는군요!");
-    break;
-  default
-    printf("당신은 아마도 과일을 좋아하지 않는 것 같군요.");
-  printf("좋아하는 과일 조사 끝");
- }
+// #include<stdio.h>
+// main(){
+//   int a;
+//   printf("1.사과 2.키위 3.복숭아 4.수박" );
+//   scanf("%d",&a);
+//   switch(a){
+//   case 1:
+//     printf("사과를 좋아하는군요!");
+//     break;
+//   case 2:
+//     printf("키위를 좋아하는군요!")# 것 같군요.");
+//   printf("좋아하는 과일 조사 끝");
+//  }
+// }
